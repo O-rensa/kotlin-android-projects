7dfddce6fbe9512b9dc63826e5fc2fe2089f9d43
Renz Lendio <131181504+O-rensa@users.noreply.github.com>
2024-11-24 17:05:55 +0800

    Changed Column layout to image

--- JetpackComposeBasic/app/src/main/java/com/example/jetpackcomposebasic/MainActivity.kt
+++ JetpackComposeBasic/app/src/main/java/com/example/jetpackcomposebasic/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,10 +15,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ControlledComposition
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -42,24 +45,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column( // using the column allows the texts to be properly rendered vertically
-        // instead of being render on top of each other
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier
-            .size(400.dp)
-    ) {
-        Text(
-            text = "Hello $name!",
-            color = Color.Blue,
-            fontSize = 30.sp,
-        )
-        Text(
-            text = "Some other text",
-            color = Color.Blue,
-            fontSize = 30.sp,
-        )
-    }
+    Image(
+        painter = painterResource(id = R.drawable.ic_launcher_foreground),
+        contentDescription = null,
+        modifier = Modifier.background(Color.Black)
+    )
 }
 
 @Preview(showBackground = true)
