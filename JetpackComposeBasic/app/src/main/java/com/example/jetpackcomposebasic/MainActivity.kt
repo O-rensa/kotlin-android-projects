62783a5ec0cdd00477596e9b50a81af48674663d
Renz Lendio <131181504+O-rensa@users.noreply.github.com>
2024-11-24 17:02:37 +0800

    Added Column Layout

--- JetpackComposeBasic/app/src/main/java/com/example/jetpackcomposebasic/MainActivity.kt
+++ JetpackComposeBasic/app/src/main/java/com/example/jetpackcomposebasic/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposebasic.ui.theme.JetpackComposeBasicTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +42,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column( // using the column allows the texts to be properly rendered vertically
+        // instead of being render on top of each other
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .size(400.dp)
+    ) {
+        Text(
+            text = "Hello $name!",
+            color = Color.Blue,
+            fontSize = 30.sp,
+        )
+        Text(
+            text = "Some other text",
+            color = Color.Blue,
+            fontSize = 30.sp,
+        )
+    }
 }
 
 @Preview(showBackground = true)
