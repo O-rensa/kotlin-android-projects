0ef3f8811d3e38581b68df884a9c0961fdcf5a81
Renz Lendio <131181504+O-rensa@users.noreply.github.com>
2024-11-30 15:55:28 +0800

    Finished and cleaned calculator

--- Calculator/app/src/main/java/com/example/calculator/Calculator.kt
+++ Calculator/app/src/main/java/com/example/calculator/Calculator.kt
@@ -11,14 +11,13 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -27,67 +26,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 val buttonList: List<String> = listOf("C", "(", ")", "/", "7", "8", "9", "*", "4", "5", "6", "+",
-    "1", "2", "3", "-", "AC", "0", ".", "=");
-
-@Composable
-fun Calculator(modifier: Modifier = Modifier) {
-    Box(modifier = modifier) {
-        Column(
-            modifier = modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.End,
-        ) {
-            Text(
-                text = "123 + 123",
-                style = TextStyle(
-                    fontSize = 30.sp,
-                    textAlign = TextAlign.End,
-                ),
-                maxLines = 5,
-                overflow = TextOverflow.Ellipsis,
-            )
-
-            Spacer(modifier = Modifier.weight(1f))
-
-            Text(
-                text = "246",
-                style = TextStyle(
-                    fontSize = 60.sp,
-                    textAlign = TextAlign.End,
-                ),
-                maxLines = 2,
-            )
-
-            Spacer(modifier = Modifier.height(10.dp))
-
-            LazyVerticalGrid(
-                columns = GridCells.Fixed(4),
-            ) {
-                items(buttonList) {
-                    CalculatorButton(btn = it)
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun CalculatorButton(btn: String) {
-    Box(modifier = Modifier.padding(10.dp)) {
-        FloatingActionButton(
-            onClick = {},
-            modifier = Modifier.size(80.dp),
-            shape = CircleShape,
-            contentColor = Color.White,
-            containerColor = getColor(btn),
-        ) {
-            Text(
-                text = btn,
-                fontSize = 22.sp,
-                fontWeight = FontWeight.Bold,
-                )
-        }
-    }
-}
+    "1", "2", "3", "-", "AC", "0", ".", "=")
 
 fun getColor(btn: String): Color {
     if ("C" == btn || "AC" == btn) {
@@ -102,5 +41,69 @@ fun getColor(btn: String): Color {
         return Color(0xFFFF9800)
     }
 
-    return Color(0xFF00C8C9);
+    return Color(0xFF00C8C9)
+}
+
+@Composable
+fun Calculator(modifier: Modifier = Modifier, viewModel: CalculatorViewModel) {
+
+    val equationText = viewModel.equationText.observeAsState()
+    val resultText = viewModel.resultText.observeAsState()
+
+    Box(modifier = modifier) {
+        Column(
+            modifier = modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.End,
+        ) {
+            Text(
+                text = equationText.value?: "",
+                style = TextStyle(
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.End,
+                ),
+                maxLines = 5,
+                overflow = TextOverflow.Ellipsis,
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Text(
+                text = resultText.value?: "",
+                style = TextStyle(
+                    fontSize = 60.sp,
+                    textAlign = TextAlign.End,
+                ),
+                maxLines = 2,
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(4),
+            ) {
+                items(buttonList) {
+                    CalculatorButton(btn = it, onClick = { viewModel.onButtonClick(it) })
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CalculatorButton(btn: String, onClick: () -> Unit) {
+    Box(modifier = Modifier.padding(10.dp)) {
+        FloatingActionButton(
+            onClick = onClick,
+            modifier = Modifier.size(80.dp),
+            shape = CircleShape,
+            contentColor = Color.White,
+            containerColor = getColor(btn),
+        ) {
+            Text(
+                text = btn,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
+                )
+        }
+    }
 }
